b2c6b532c0c85896b0d05e18222b9f4e1e856485
Jiaxing Qiu <jx.qiu@outlook.com>
2024-12-09 16:58:23 +0800

    Revert "move gbuffer dimension mismatch detection to glTexImage2D"
    This reverts commit 551250fd6409949e2e53a33365015ef78e617f90.

--- hw/express-gpu/glv3_status.c
+++ hw/express-gpu/glv3_status.c
@@ -480,12 +480,22 @@ void d_glBindEGLImage(void *t_context, GLenum target, uint64_t image, GLuint tex
             status->current_external_gbuffer = gbuffer;
         }
 
+        // glEGLImageTargetTexture needs to perform a check to make sure its
+        // underlying texture storage is of the same size as the specs of the image
+        int w, h;
         GLuint prev_texture = 0;
         GLuint prev_unpack = 0;
         glGetIntegerv(GL_TEXTURE_BINDING_2D, (GLint *)&prev_texture);
         glGetIntegerv(GL_PIXEL_UNPACK_BUFFER_BINDING, (GLint *)&prev_unpack);
         glBindTexture(GL_TEXTURE_2D, gbuffer->data_texture);
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+        glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &w);
+        glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &h);
+
+        if (gbuffer->width != w || gbuffer->height != h) {
+            // isolate previous storage and reallocate storage of matching specs
+            glTexImage2D(GL_TEXTURE_2D, 0, gbuffer->internal_format, gbuffer->width, gbuffer->height, 0, gbuffer->format, gbuffer->pixel_type, NULL);
+        }
 
         if (gbuffer->pref_phy_dev != EXPRESS_MEM_TYPE_UNKNOWN && gbuffer->pref_phy_dev != EXPRESS_MEM_TYPE_TEXTURE) {
             // oops, prefetch failure, sync gpu mem
