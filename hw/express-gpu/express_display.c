d387f625e235a107bd1f52cc44955719cd2db457
Jiaxing Qiu <jx.qiu@outlook.com>
2025-04-10 20:19:01 +0800

    bugfix: display log signal 11

--- hw/express-gpu/express_display.c
+++ hw/express-gpu/express_display.c
@@ -583,7 +583,7 @@ static void opengl_paint_composer_layers(Display_Context *disp, GBuffer_Layers *
                 opengl_paint_gbuffer(gbuffer);
             }
             else {
-                LOGW("display %s: cannot find layer gbuffer %llx", disp->unique_id, layer.gbuffer_id);
+                LOGW("display %s: cannot find layer gbuffer %llx", disp->info.name, layer.gbuffer_id);
             }
             LOGD("composer set sync %d", layer.read_sync_id);
             signal_express_sync(layer.read_sync_id, true);
