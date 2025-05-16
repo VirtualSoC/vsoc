e8beffdf283eccafe5e433983bfb46e048afa34c
lluviasnow <191250215@smail.nju.edu.cn>
2025-05-16 14:14:23 +0800

    add simple handle mapping

--- hw/express-gpu/express_vk_handle_mapping.c
+++ hw/express-gpu/express_vk_handle_mapping.c
@@ -1,4 +1,46 @@
 #include "hw/express-gpu/express_vk_handle_mapping.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+
+typedef struct {
+    ExpressVkObjectType type; // 指明是哪种 Vulkan 对象
+    uint64_t guest_id;        // 来自 Guest 的伪句柄编号
+    uint64_t host_id;         // 真正的 Host 句柄值（uint64_t 表示）
+} ExpressObjectEntry;
+
+// 全局映射表（简化版，无 is_init）
+ExpressObjectEntry *g_entries = NULL;
+size_t g_entry_count = 0;
+size_t g_entry_capacity = 0;
+
+int ensure_capacity(size_t min_capacity) {
+    if (min_capacity <= g_entry_capacity)
+        return 1;
+    size_t new_cap = g_entry_capacity ? g_entry_capacity * 2 : 16;
+    if (new_cap < min_capacity) new_cap = min_capacity;
+    ExpressObjectEntry *new_buf = realloc(g_entries, new_cap * sizeof(*new_buf));
+    if (!new_buf) return 0;
+    g_entries = new_buf;
+    g_entry_capacity = new_cap;
+    return 1;
+}
+
+int insert_mapping(ExpressVkObjectType type, uint64_t guest_id, uint64_t host_id) {
+    if (!ensure_capacity(g_entry_count + 1)) return 0;
+    g_entries[g_entry_count++] = (ExpressObjectEntry){ type, guest_id, host_id };
+    return 1;
+}
+
+uint64_t lookup_mapping(ExpressVkObjectType type, uint64_t guest_id) {
+    for (size_t i = 0; i < g_entry_count; ++i) {
+        if (g_entries[i].type == type && g_entries[i].guest_id == guest_id)
+            return g_entries[i].host_id;
+    }
+    return -1;
+}
 
 // map_handle_<Type> 实现：目前直接原样返回，可自行改写为真正的映射逻辑
 VkInstance map_handle_VkInstance(VkInstance boxed) {
--- hw/express-gpu/vk_trans.c
+++ hw/express-gpu/vk_trans.c
@@ -14,6 +14,7 @@
 
 #include "hw/express-mem/express_sync.h"
 #include "hw/express-gpu/express_vk_decode_from_stream.h"
+#include "hw/express-gpu/express_vk_handle_mapping.h"
 
 void vk_decode_invoke(Render_Thread_Context *context, Teleport_Express_Call *call)
 
@@ -48,13 +49,12 @@ void vk_decode_invoke(Render_Thread_Context *context, Teleport_Express_Call *cal
         VkInstance pInstance;
     
         int para_num = get_para_from_call(call, all_para, MAX_PARA_NUM);
-        LOGI("get vk param number %d", para_num);
+        LOGI("get vk param number %d instance is %lld %lld", para_num, pInstance, &pInstance);
 
         int need_free = 0;
         char *stream_ptr;
         stream_ptr = call_para_to_ptr(all_para[0], &need_free);
         uint8_t ** stream_ptr_ptr = (uint8_t **)&stream_ptr;
-        //拿到这个指针之后需要解码！！！
 
         // VkInstanceCreateInfo* pCreateInfo = (VkInstanceCreateInfo*)_ptr;
         decode_from_stream_VkInstanceCreateInfo(VK_STRUCTURE_TYPE_MAX_ENUM, (VkInstanceCreateInfo*)(pCreateInfo), stream_ptr_ptr); 
@@ -62,12 +62,30 @@ void vk_decode_invoke(Render_Thread_Context *context, Teleport_Express_Call *cal
         LOGI("got vkCreateinfo with %lld %d %s %d %s",(long long)pCreateInfo->sType, pCreateInfo->enabledLayerCount, pCreateInfo->ppEnabledLayerNames, pCreateInfo->enabledExtensionCount, pCreateInfo->ppEnabledExtensionNames);
         LOGI("application create info is %lld %d",(long long)pCreateInfo->pApplicationInfo, pCreateInfo->pApplicationInfo->sType);
 
-        VkResult result = vkCreateInstance(pCreateInfo, pAllocator, &pInstance);
-        LOGI("got result!");
-        LOGI("got result %d instance %lld", result, pInstance);
+        VkAllocationCallbacks* guest_allocator = (VkAllocationCallbacks*)(**stream_ptr_ptr);
+        *stream_ptr_ptr += 8;
 
+        if(guest_allocator) {
+            decode_from_stream_VkAllocationCallbacks(VK_STRUCTURE_TYPE_MAX_ENUM, pAllocator, stream_ptr_ptr);
+        }
+
+        uint64_t guest_instance = (uint64_t)(**stream_ptr_ptr);
+        *stream_ptr_ptr += 8;
+
+        VkResult result = vkCreateInstance(pCreateInfo, pAllocator, &pInstance);
+
+        if (result == VK_SUCCESS) {
+            LOGI("got result %d instance %lld %lld size %d guest %lld", result, pInstance, &pInstance, sizeof(VkInstance), guest_instance);
+            insert_mapping(EXPRESS_VK_OBJECT_TYPE_INSTANCE, guest_instance, (uint64_t)(uintptr_t)pInstance);
+            LOGI("map result is %lld", lookup_mapping(EXPRESS_VK_OBJECT_TYPE_INSTANCE, guest_instance));
+
+            
+
+        }
+        write_to_guest_mem(all_para[1].data, &result, 0, sizeof(VkResult));
+        // return;
     }
     break;
     }
-
+    call->callback(call, 1);
 }
