8f76b8bb609d2354148f744a136471f4bb06525c
Jiaxing Qiu <jx.qiu@outlook.com>
2025-06-10 14:31:10 +0800

    EGL surfaceless context support (on linux)

--- hw/express-gpu/egl_window_egl.c
+++ hw/express-gpu/egl_window_egl.c
@@ -1,75 +1,48 @@
 #include "hw/teleport-express/express_log.h"
+#include "hw/teleport-express/express_device_common.h"
 #include "hw/express-gpu/egl_window.h"
 
 #include <glib.h>
 #include <dlfcn.h>
 #include <stdio.h>
 
+#include "hw/express-gpu/egl.h"
+
 typedef void (*EGLproc)(void);
 
-typedef int EGLint;
-typedef unsigned int EGLBoolean;
-typedef unsigned int EGLenum;
-typedef void *EGLConfig;
-typedef void *EGLContext;
-typedef void *EGLDisplay;
-typedef void *EGLSurface;
-
-#define EGL_NONE 0x3038 /* Attrib list terminator */
-
-#define EGL_TRUE 1
-#define EGL_FALSE 0
-
-#define EGL_CONTEXT_MAJOR_VERSION 0x3098
-#define EGL_CONTEXT_MINOR_VERSION 0x30FB
-#define EGL_WIDTH 0x3057
-#define EGL_HEIGHT 0x3056
-
-#define EGL_ALPHA_SIZE 0x3021
-#define EGL_BLUE_SIZE 0x3022
-#define EGL_GREEN_SIZE 0x3023
-#define EGL_RED_SIZE 0x3024
-#define EGL_DEPTH_SIZE 0x3025
-#define EGL_STENCIL_SIZE 0x3026
-
-#if defined(__cplusplus)
-#define EGL_CAST(type, value) (static_cast<type>(value))
-#else
-#define EGL_CAST(type, value) ((type)(value))
-#endif
-
-#define EGL_NO_CONTEXT EGL_CAST(EGLContext, 0)
-#define EGL_NO_DISPLAY EGL_CAST(EGLDisplay, 0)
-#define EGL_NO_SURFACE EGL_CAST(EGLSurface, 0)
-
-typedef EGLBoolean (*PFN_eglGetConfigAttrib)(EGLDisplay, EGLConfig, EGLint, EGLint *);
-typedef EGLBoolean (*PFN_eglGetConfigs)(EGLDisplay, EGLConfig *, EGLint, EGLint *);
+typedef EGLproc (*PFN_eglGetProcAddress)(const char *procname);
 typedef EGLint (*PFN_eglGetError)(void);
 typedef EGLBoolean (*PFN_eglInitialize)(EGLDisplay, EGLint *, EGLint *);
 typedef EGLBoolean (*PFN_eglTerminate)(EGLDisplay);
 typedef EGLBoolean (*PFN_eglBindAPI)(EGLenum);
-
+typedef char const * (*PFN_eglQueryString)(EGLDisplay display, EGLint name);
 typedef EGLContext (*PFN_eglCreateContext)(EGLDisplay, EGLConfig, EGLContext, const EGLint *);
-
 typedef EGLBoolean (*PFN_eglChooseConfig)(EGLDisplay, const EGLint *, EGLConfig *, EGLint, EGLint *);
 typedef EGLSurface (*PFN_eglCreatePbufferSurface)(EGLDisplay, EGLConfig, const EGLint *);
 typedef EGLBoolean (*PFN_eglDestroySurface)(EGLDisplay, EGLSurface);
 typedef EGLBoolean (*PFN_eglDestroyContext)(EGLDisplay, EGLContext);
 typedef EGLBoolean (*PFN_eglMakeCurrent)(EGLDisplay, EGLSurface, EGLSurface, EGLContext);
-typedef EGLBoolean (*PFN_eglSwapInterval)(EGLDisplay, EGLint);
 
-PFN_eglInitialize eglInitialize;
-PFN_eglMakeCurrent eglMakeCurrent;
-PFN_eglCreateContext eglCreateContext;
-PFN_eglCreatePbufferSurface eglCreatePbufferSurface;
-PFN_eglDestroySurface eglDestroySurface;
-PFN_eglDestroyContext eglDestroyContext; 
-PFN_eglChooseConfig eglChooseConfig;
-PFN_eglGetError eglGetError;
+typedef struct EglFunctions {
+    PFN_eglGetProcAddress eglGetProcAddress;
+    PFN_eglInitialize eglInitialize;
+    PFN_eglQueryString eglQueryString;
+    PFN_eglMakeCurrent eglMakeCurrent;
+    PFN_eglCreateContext eglCreateContext;
+    PFN_eglCreatePbufferSurface eglCreatePbufferSurface;
+    PFN_eglDestroySurface eglDestroySurface;
+    PFN_eglDestroyContext eglDestroyContext; 
+    PFN_eglChooseConfig eglChooseConfig;
+    PFN_eglGetError eglGetError;
+} EglFunctions;
+
+static EglFunctions platform;
 
 static EGLDisplay main_window_display;
 static EGLContext main_window_context;
 
+
+static bool supports_surfaceless_context;
 static GHashTable *context_pbuffer_map;
 
 static EGLConfig static_config;
@@ -111,20 +84,27 @@ static EGLproc load_egl_fun(const char *name)
     return ret;
 }
 
-#define LOAD_EGL_FUN(name)                            \
-    name = (PFN_##name)load_egl_fun(#name);           \
-    if (name == NULL)                                 \
-        name = (PFN_##name)load_egl_fun(#name "ARB"); \
-    if (name == NULL)                                 \
-        name = (PFN_##name)load_egl_fun(#name "KHR"); \
-    if (name == NULL)                                 \
-        name = (PFN_##name)load_egl_fun(#name "EXT"); \
-    if (name == NULL)                                 \
-        printf("cannot find %s", #name);
+#define LOAD_EGL_FUN(name) \
+    platform.name = (PFN_##name)platform.eglGetProcAddress(#name); \
+    if (platform.name == NULL) \
+        platform.name = (PFN_##name)platform.eglGetProcAddress(#name "ARB"); \
+    if (platform.name == NULL) \
+        platform.name = (PFN_##name)platform.eglGetProcAddress(#name "KHR"); \
+    if (platform.name == NULL) \
+        platform.name = (PFN_##name)platform.eglGetProcAddress(#name "EXT"); \
+    if (platform.name == NULL) \
+        printf("eglGetProcAddress failed to load %s\n", #name);
 
 void egl_init(void *dpy, void *father_context)
 {
+    platform.eglGetProcAddress = (PFN_eglGetProcAddress)load_egl_fun("eglGetProcAddress");
+    if (platform.eglGetProcAddress == NULL) {
+        LOGE("error! eglGetProcAddress not found, cannot load EGL functions");
+        return;
+    }
+
     LOAD_EGL_FUN(eglInitialize);
+    LOAD_EGL_FUN(eglQueryString);
     LOAD_EGL_FUN(eglMakeCurrent);
     LOAD_EGL_FUN(eglCreateContext);
     LOAD_EGL_FUN(eglCreatePbufferSurface);
@@ -138,11 +118,26 @@ void egl_init(void *dpy, void *father_context)
     main_window_display = (EGLDisplay)dpy;
     main_window_context = (EGLContext)father_context;
 
-    if (!eglInitialize(main_window_display, NULL, NULL))
+    if (!platform.eglInitialize(main_window_display, NULL, NULL))
     {
-        LOGE("eglInitialize error %x display %p context %p", eglGetError(), main_window_display, main_window_context);
+        LOGE("eglInitialize error %x display %p context %p", platform.eglGetError(), main_window_display, main_window_context);
     }
 
+    const char *version = platform.eglQueryString(main_window_display, EGL_VERSION);
+    const char *vendor = platform.eglQueryString(main_window_display, EGL_VENDOR);
+    const char *extensions = platform.eglQueryString(main_window_display, EGL_EXTENSIONS);
+    LOGI("EGL version: %s, vendor: %s", version ? version : "unknown", vendor ? vendor : "unknown");
+    if (express_gpu_gl_debug_enable)
+    {
+        LOGI("EGL extensions: %s", extensions ? extensions : "none");
+    }
+
+    if (strstr(extensions, "EGL_KHR_surfaceless_context") != NULL)
+    {
+        supports_surfaceless_context = true;
+        LOGD("EGL_KHR_surfaceless_context is supported, will create surfaceless contexts");
+    }
+    
     EGLint attrib_list[] = {
         EGL_RED_SIZE, 8,
         EGL_GREEN_SIZE, 8,
@@ -154,35 +149,40 @@ void egl_init(void *dpy, void *father_context)
     };
 
     EGLint num_configs = 0;
-    if (!eglChooseConfig(main_window_display, attrib_list, &static_config, 1, &num_configs) || num_configs == 0)
+    if (!platform.eglChooseConfig(main_window_display, attrib_list, &static_config, 1, &num_configs) || num_configs == 0)
     {
-        LOGE("choose config error %x display %p context %p", eglGetError(), main_window_display, main_window_context);
+        LOGE("choose config error %x display %p context %p", platform.eglGetError(), main_window_display, main_window_context);
     }
+
+    g_mutex_init(&main_window_mutex);
 }
 
 void *egl_createContext(int context_flags)
 {
     g_mutex_lock(&main_window_mutex);
 
-    EGLContext context = eglCreateContext(main_window_display, static_config, main_window_context, static_context_attribs);
+    EGLContext context = platform.eglCreateContext(main_window_display, static_config, main_window_context, static_context_attribs);
     if (context == EGL_NO_CONTEXT)
     {
-        LOGE("error! eglCreateContext failed with error 0x%x main_window_display %p main_window_context %p", eglGetError(), main_window_display, main_window_context);
+        LOGE("error! eglCreateContext failed with error 0x%x main_window_display %p main_window_context %p", platform.eglGetError(), main_window_display, main_window_context);
         g_mutex_unlock(&main_window_mutex);
         return EGL_NO_CONTEXT;
     }
 
-    EGLSurface pbuffer = eglCreatePbufferSurface(main_window_display, static_config, static_pbuffer_attribs);
+    if (!supports_surfaceless_context) {
+        EGLSurface pbuffer = platform.eglCreatePbufferSurface(main_window_display, static_config, static_pbuffer_attribs);
 
-    if (pbuffer == EGL_NO_SURFACE)
-    {
-        LOGE("error! eglCreatePbufferSurface failed with error 0x%x main_window_display %p main_window_context %p", eglGetError(), main_window_display, main_window_context);
-        eglDestroyContext(main_window_display, context);
-        g_mutex_unlock(&main_window_mutex);
-        return EGL_NO_CONTEXT;
+        if (pbuffer == EGL_NO_SURFACE)
+        {
+            LOGE("error! eglCreatePbufferSurface failed with error 0x%x main_window_display %p main_window_context %p", platform.eglGetError(), main_window_display, main_window_context);
+            platform.eglDestroyContext(main_window_display, context);
+            g_mutex_unlock(&main_window_mutex);
+            return EGL_NO_CONTEXT;
+        }
+
+        g_hash_table_insert(context_pbuffer_map, (gpointer)context, pbuffer);
     }
 
-    g_hash_table_insert(context_pbuffer_map, (gpointer)context, pbuffer);
     g_mutex_unlock(&main_window_mutex);
     return context;
 }
@@ -190,24 +190,26 @@ void *egl_createContext(int context_flags)
 int egl_makeCurrent(void *context)
 {
     int result;
-    EGLSurface pbuffer;
+    EGLSurface pbuffer = EGL_NO_SURFACE;
 
     g_mutex_lock(&main_window_mutex);
 
     if (context != NULL)
     {
-        pbuffer = g_hash_table_lookup(context_pbuffer_map, (gpointer)context);
-        result = eglMakeCurrent(main_window_display, pbuffer, pbuffer, context);
+        if (!supports_surfaceless_context) {
+            pbuffer = g_hash_table_lookup(context_pbuffer_map, (gpointer)context);
+        }
+        result = platform.eglMakeCurrent(main_window_display, pbuffer, pbuffer, context);
     }
     else
     {
-        pbuffer = EGL_NO_SURFACE;
-        result = eglMakeCurrent(main_window_display, pbuffer, pbuffer, EGL_NO_CONTEXT);
+        result = platform.eglMakeCurrent(main_window_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
     }
+
     g_mutex_unlock(&main_window_mutex);
 
     if (result != EGL_TRUE) {
-        LOGE("error! eglMakeCurrent failed with error 0x%x main_window_display %p context %p pbuffer %p", eglGetError(), main_window_display, context, pbuffer);
+        LOGE("error! eglMakeCurrent failed with error 0x%x main_window_display %p context %p pbuffer %p", platform.eglGetError(), main_window_display, context, pbuffer);
         return EGL_FALSE;
     }
 
@@ -219,13 +221,15 @@ void egl_destroyContext(void *context)
     if (context != NULL)
     {
         g_mutex_lock(&main_window_mutex);
-        
-        EGLSurface pbuffer = g_hash_table_lookup(context_pbuffer_map, (gpointer)context);
 
-        eglDestroyContext(main_window_display, context);
-        eglDestroySurface(main_window_display, pbuffer);
+        platform.eglDestroyContext(main_window_display, context);
 
-        g_hash_table_remove(context_pbuffer_map, (gpointer)context);
+        if (!supports_surfaceless_context)
+        {
+            EGLSurface pbuffer = g_hash_table_lookup(context_pbuffer_map, (gpointer)context);
+            platform.eglDestroySurface(main_window_display, pbuffer);
+            g_hash_table_remove(context_pbuffer_map, (gpointer)context);
+        }
         
         g_mutex_unlock(&main_window_mutex);
     }
